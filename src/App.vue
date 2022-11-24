0d46dd9e5f0c205cae60b46a3e13af06d70b4481
MattiaLongo03 <mattilongo2003@gmail.com>
2022-11-24 17:30:51 +0100

    header and content

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HeaderTop></HeaderTop>
+    <ContentPage></ContentPage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderTop from '@/components/HeaderTop.vue';
+import ContentPage from '@/components/ContentPage.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HeaderTop,
+    ContentPage,
+  },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+ *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+ }
 </style>
---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="../assets/dc-logo.png" alt="">
+    </div>
+    <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  header{
+    display: flex;
+    justify-content: space-between;
+    .logo{
+      margin-left: 50px;
+    }
+    ul{
+      margin-right: 200px;
+      display: flex;
+      list-style: none;
+      align-items: center;
+    }
+    li{
+      padding-right: 30px;
+      font-weight: bold;
+    }
+  }
+</style>
