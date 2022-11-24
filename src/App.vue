f0fa4cc5f09435f94516d1b203f06651bf7ee115
MattiaLongo03 <mattilongo2003@gmail.com>
2022-11-24 18:45:49 +0100

    Footer_2

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <ContentPage></ContentPage>
     <BlueLine></BlueLine>
     <Footer_1></Footer_1>
+    <Footer_2></Footer_2>
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import HeaderTop from '@/components/HeaderTop.vue';
 import ContentPage from '@/components/ContentPage.vue';
 import BlueLine from '@/components/BlueLine.vue';
 import Footer_1 from '@/components/Footer_1.vue';
+import Footer_2 from '@/components/Footer_2.vue';
 
 export default {
   components: {
@@ -19,14 +21,15 @@ export default {
     ContentPage,
     BlueLine,
     Footer_1,
+    Footer_2,
   },
 };
 </script>
 
 <style lang="scss" scoped>
  *{
-    padding: 0;
     margin: 0;
+    padding: 0;
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
  }
---
+++ src/components/Footer_2.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <div class="sign-up">
+            <p>SIGN-UP NOW!</p>
+        </div>
+
+        <div class="follow">
+            <span>FOLLOW US</span>
+            <img src="../assets/footer-facebook.png" alt="">
+            <img src="../assets/footer-twitter.png" alt="">
+            <img src="../assets/footer-youtube.png" alt="">
+            <img src="../assets/footer-pinterest.png" alt="">
+            <img src="../assets/footer-periscope.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    img{
+        padding: 10px;
+    }
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #303030;
+    }
+    .sign-up{
+        padding: 3px;
+        margin-left: 50px;
+        color: white;
+        border: solid 3px #0282F9;
+    }
+    .follow{
+        display: flex;
+        align-items: center;
+        span{
+            margin-right: 5px;
+            color: #0282F9;
+            font-weight: bold;
+        }
+    }
+</style>
